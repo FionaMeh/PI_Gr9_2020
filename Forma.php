654cceff2a88e353f32d130e8c202bb3d4938a10
erisera19 <58450146+erisera19@users.noreply.github.com>
2020-05-06 23:35:54 +0200

    Modifikime ne faqen per validimin e formave

--- Forma.php
+++ Forma.php
@@ -4,23 +4,52 @@
    if(isset($_POST['submit-send']))
    {
        $name = $_POST['name'];
-       $email=$_POST['email'];
-       $message=$_POST['message'];
+       $email = $_POST['email'];
+       $message = $_POST['message'];
+       $string_exp = "/^[A-Za-z .'-]+$/";
 
-       if(empty($name) || empty($email) || empty($message))
-       {
+
+       if (empty($name) || empty($email) || empty($message)) {
            header('location:Contact.php?error');
-       }
-       else {
-           $recipent="eriseratr@gmail.com";
-           $formcontent="From: $name \n Message: $message";
-           $mailHeader="From: $email";
-           mail($recipent, $formcontent, $mailHeader);
-           echo "Mesazhi u dergua";
-       }
+       } elseif (!isValidEmail($email)) {
+           died(' The email you entered is incorrect');
+       } elseif (!preg_match($string_exp, $name) || (strlen($name) < 3)) {
+           died(' The username you entered is incorrect');
+       } else {
+           $recipent = "eriseratr@gmail.com";
+           $formcontent = "From: $name \n Message: $message";
+           $mailHeader = "From: $email";
+           if (mail($recipent, $formcontent, $mailHeader)) {
+               print "<h3>Mesazhi juaj u pranua</h3>";
+           } else {
+               print "<h3>Mesazhi juaj nuk u pranua, ju lutem provoni me vone</h3>";
+           }
 
-
+       }
 
    }
+
+
+function isValidEmail($imella)
+{
+    return preg_match('/\A[a-z0-9]+([-._][a-z0-9]+)*@([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,4}\z/', $imella)
+        && preg_match('/^(?=.{1,64}@.{4,64}$)(?=.{6,100}$).*/', $imella);
+}
+
+function died($error)
+{
+    echo '<script type="text/javascript">alert("We are very sorry, but there were error(s) found with the form you submitted.' . $error . ' ");</script>';
+}
+
+if (isset($_POST['submit-rate'])) {
+    $teksti = $_POST['teksti'];
+    if (($teksti == "") || (!is_numeric($teksti)) || ($teksti < 1) || ($teksti > 10)) {
+        echo "Not possible";
+    } else {
+        echo "Faleminderit qe votuat";
+    }
+
+}
+
 
 
