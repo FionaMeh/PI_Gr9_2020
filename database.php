d9e0800792b6b1853cf5743e1a9e097c692348a7
erisera19 <58450146+erisera19@users.noreply.github.com>
2020-05-13 17:44:13 +0200

    database

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$con = mysqli_connect("localhost", "root", "", "udhetime");
+mysqli_set_charset($con, "utf8");
+
+if (mysqli_connect_errno()) {
+    echo "Gabim. Provoni me vone.";
+}
+?>
+
+
+
