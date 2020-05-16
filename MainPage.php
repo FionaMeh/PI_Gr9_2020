90a4cf8d558b267b70d22914085ddd3d16025bc0
erisera19 <58450146+erisera19@users.noreply.github.com>
2020-05-16 02:04:24 +0200

    Faqja kryesore

--- MainPage.php
+++ MainPage.php
@@ -49,42 +49,7 @@
             }
         }
     </script>
-	<script>
-		<!-- FooterValidation - javascript -->
-		function validate()
-	     		
-	     		{
-	     			var emri=document.getElementById('emri').value;
-	     			var email=document.getElementById('imella').value;
-	     			var a=document.footerform.Email.value;
-	     			
-	     		
-	     			
-	     		
-	     		if((emri=="")&&(email==""))
-	     		{
-	     			document.getElementById('errorinfooter').innerHTML="All fields must be filled!";
-	     			return false;
-	     		}
-	     		if(a.indexOf('@')<=0)
-	     			{
-	     				document.getElementById('emailerror').innerHTML="Invalid @ position!";
-	     				return false;
 
-	     			}
-	     			else if((a.charAt(a.length-4)!='.') &&(a.charAt(a.length-3)!='.'))
-	     			{
-	     				document.getElementById('emailerror').innerHTML="Invalid . position at 4!"   ;
-	     				return false;
-	     			}
-	     			
-	     		else
-	     		{
-	     			return true;
-	     		}
-}
-
-	</script>
    
 	 </head>
 	
@@ -113,143 +78,140 @@
 		</div>
 	</div>
 	<!-- Slider - javascript -->
-	<script type="text/javascript">
-		var slider_content = document.getElementById('box');
+     <script type="text/javascript">
+         var slider_content = document.getElementById('box');
 
-		var image = ['syriKalter','visitalbania1','valbona','visitalbania4'];
+         var image = ['syriKalter', 'visitalbania1', 'valbona', 'visitalbania4'];
 
-		var i = image.length;
+         var i = image.length;
 
-		function nextImage() {
-			if(i < image.length) {
-				i = i + 1;
-			}else {
-				i = 1;
-			}
-			slider_content.innerHTML = "<img class='fiona' src=images/"+image[i-1]+".jpg>"; 
-		}
+         function nextImage() {
+             if (i < image.length) {
+                 i = i + 1;
+             } else {
+                 i = 1;
+             }
+             slider_content.innerHTML = "<img class='fiona' src=images/" + image[i - 1] + ".jpg>";
+         }
 
-		function prevImage() {
-			if(i < image.length+1 && i>1) {
-				i = i - 1;
-			}else {
-				i = image.length;
-			}
-			slider_content.innerHTML = "<img class='fiona' src=images/"+image[i-1]+".jpg>"; 
-		}
-	
-
-	</script>
-	
-	<br>
-	<br>
-	<br>
-	<div>
-		<table>
-			<tr>
-				<td style="width:44%" style="color:black;" ><hr/></td>
-				<td text-align: center style="color:black;">Castle</td>
-				<td style="width:55%"><hr/></td>
-			</tr>
-		</table>
+         function prevImage() {
+             if (i < image.length + 1 && i > 1) {
+                 i = i - 1;
+             } else {
+                 i = image.length;
+             }
+             slider_content.innerHTML = "<img class='fiona' src=images/" + image[i - 1] + ".jpg>";
+         }
 
 
-	  <div id="o" class="row">
-        <div id="o" class="column">
-            <img src="images/gjirokaster.jpg" alt="Avatar"  style="width:360px;height:160px" style="width:100%" />
-            <div class="longtext">
-                <h1 style="color:black;">Gjirokastër Castle</h1>
-				<p style="color:grey;" class="show-ellipsis">Gjirokastër Castle is a fortress in Gjirokastër, 
-					Albania (during Ottoman rule it was historically known as Ergiri while local Greeks referred to it as Argyrokastro, 
-					a name applied also to the castle).<br> Gjirokastër Castle is situated at a height of 336 metres).
-					The castle dominates the town and overlooks the strategically important route along the river valley.
-					 It is open to visitors and contains a military museum featuring captured artillery and memorabilia 
-					 of the Communist resistance against German occupation, <br>as well as a captured United States Air Force 
-					 plane to commemorate the Communist regime's struggle against the imperialist western powers.</p>
-            </div>
-            <button onclick="showText(this)" class="class_search3" class="moreButton">READ MORE</button>
-        </div>
+     </script>
 
-        <div id="o" class="column">
-            <img src="images/kruje.jpg" alt="Avatar" style="width:360px;height:160px" style="width:100%" />
-            <div class="longtext">
-
-                <h1 style="color:black;">Krujë Castle</h1>
-				<p style="color:grey;" class="show-ellipsis">Krujë castle was built in the 5th or 6th century,
-					 perched above the city of the same name. At the time of the Ottoman threat, it was the headquarters
-					  of Gjergj Kastrioti, the “Dragon of Albania,” an Albanian nobleman and military commander.
-					   Kastrioti was sent as a hostage to the Ottoman <br> court early in his life, and after leaving service, 
-					   led a rebellion against the Ottoman Empire in Albania.</p>
-
-            </div>
-            <button onclick="showText(this)" class="class_search3" class="moreButton">READ MORE</button>
-        </div>
-
-        <div id="o" class="column">
-            <img src="images/rozafa (1).jpg" style="width:360px;height:160px" alt="Avatar" style="width:100%" />
-            <div class="longtext">
-                <h1 style="color:black;">Rozafa Castle</h1>
-				<p style="color:grey;" class="show-ellipsis">From its ancient Illyrian walls and the medieval ruins of the Balshaj,
-					 to the Towers of the Bushatllinj, Rozafa Castle is a monumental testament to the beauty of an everlasting fortress.
-					Built during Antiquity,<br> enduring the Roman and Ottoman empires, and remaining intact up to the present day,
-					 Rozafa Castle has witnessed the dramatic changes of tide over the course Albania’s fascinating history. 
-					 Needless to say then that this archaeological area is incredibly rich in historical tales.
-					  To understand the city of Shkodra, where the castle is located, and indeed the history of the country as a whole,
-					   a visit to Rozafa Castle is a must!</p>
-            </div>
-            <button onclick="showText(this)" class="class_search3" class="moreButton">READ MORE</button>
-        </div>
+     <br>
+     <br>
+     <br>
+     <h2>Castles</h2>
+     <?php include "database.php" ?>
+     <?php
+     $sql = "SELECT * FROM keshtjellat";
 
 
-    </div>
-	 <table>
-	 	<tr>
-	 		
-			 <td style="width:20%"><hr/></td>
-			 <td text-align: center style="color:black;">Summer Sports</td>
-			 <td style="width:20%"><hr/></td>
-			 <td style="width:26%" ><hr/></td>
-			 <td text-align: center style="color:black;"> Hiking </td>
-			 <td style="width:22%"><hr/></td>
-		 </tr>
-	 </table>
-	 
-	 <table id="w">
-		<tr >
-			<td style="text-align:left" >
-				<img class="img-round" src="images/jetblade_new.jpg" alt="Avatar" align="left"  style="width:128px;height:128px" hspace="20" />
-				<p style="color:black;"><strong>Surfing,jet skiing and more</strong></p>
-				<div class="content1" style=" padding:0; margin:0; ">
-					<div class="read-more" style=" padding:0; margin:0; ">
-				<p style="color:grey;">Maybe it's on your bucket list or maybe not.<br> 
-										 But, it still sounds fun and for those that 
-										 live by the ocean and do surf this is on here for you.</p>
-				   </div>
-			   </div>
-			</td>
-			<td style="text-align:left" style=" padding:0; margin:0; " > 
-				<img  class="square" src="images/albania-trekking-from-valbona-to-theth.jpg" alt="Avatar" align="left" style="width:110px;height:130px" hspace="20" />
-				<p style="color:black;"><strong>Mountain Climbing</strong</hp>
-				<div class="content1" style=" padding:0; margin:0; " >
-					<div class="read-more" style=" padding:0; margin:0; ">
-				<p style="color:grey;">Kind of an extreme sport and definitely not for the faint of heart.<br> 
-										But, if you live for adventure then maybe climbing a mountain of
-										 a low or high peak is exactly what you want to do. </p>
-									   </div>
-								   </div >
-								   
+     $rezultati = mysqli_query($con, $sql);
 
-			</td>
-		</tr>
-		<tr style="text-align:left">
-			<td>
-				<img class="img-round" src="images/kayaking.jpg" alt="Avatar" align="left" style="width:128px;height:128px" hspace="20" />
-				<p style="color:black;"><strong>Kayaking</strong></p>
-				<div class="content1" style=" padding:0; margin:0; ">
-					<div class="read-more" style=" padding:0; margin:0; "> 
-				<p style="color:grey;"> Again, for those that love adventure.<br>                 
-										  Once you try kayaking, we’re positive you’ll love it.
-										  Kayaking provides a number of
+     if ($rezultati !== false) {
+     if ($rezultati->num_rows > 0) {
+     while ($rreshti = mysqli_fetch_assoc($rezultati)) {
+     $rreshti_titulli = $rreshti['titulli'];
+     $rreshti_teksti = $rreshti['teksti'];
+     $rreshti_foto = $rreshti['foto']
+
+     ?>
+
+
+     <div id="o" class="row">
+         <div id="o" class="column">
+             <img src="images/<?php echo $rreshti_foto; ?>" alt="Avatar" style="width:360px;height:160px"
+                  style="width:100%"/>
+             <div class="longtext">
+                 <h2 style="color:black;"><?= $rreshti_titulli; ?></h2>
+                 <p style="color:grey;" class="show-ellipsis"><?= $rreshti_teksti ?></p>
+             </div>
+             <button onclick="showText(this)" class="class_search3" class="moreButton">READ MORE</button>
+
+         </div>
+
+
+         <?php } ?>
+
+         <?php
+         } else {
+             echo "Nuk ka asnje lajm me kete ID";
+         }
+         } else {
+             echo "Gabim gjate leximit te lajmit";
+
+         }
+         ?>
+     </div>
+
+     <table>
+         <tr>
+
+             <td style="width:20%">
+                 <hr/>
+             </td>
+             <td style="text-align: center; color:black;">Summer Sports</td>
+             <td style="width:20%">
+                 <hr/>
+             </td>
+             <td style="width:26%">
+                 <hr/>
+             </td>
+             <td text-align: center style="color:black;"> Hiking</td>
+             <td style="width:22%">
+                 <hr/>
+             </td>
+         </tr>
+     </table>
+
+     <table id="w">
+         <tr>
+             <td style="text-align:left">
+                 <img class="img-round" src="images/jetblade_new.jpg" alt="Avatar" align="left"
+                      style="width:128px;height:128px" hspace="20"/>
+                 <p style="color:black;"><strong>Surfing,jet skiing and more</strong></p>
+                 <div class="content1" style=" padding:0; margin:0; ">
+                     <div class="read-more" style=" padding:0; margin:0; ">
+                         <p style="color:grey;">Maybe it's on your bucket list or maybe not.<br>
+                             But, it still sounds fun and for those that
+                             live by the ocean and do surf this is on here for you.</p>
+                     </div>
+                 </div>
+             </td>
+             <td style="text-align:left" style=" padding:0; margin:0; ">
+                 <img class="square" src="images/albania-trekking-from-valbona-to-theth.jpg" alt="Avatar" align="left"
+                      style="width:110px;height:130px" hspace="20"/>
+                 <p style="color:black;"><strong>Mountain Climbing</strong</p>
+                 <div class="content1" style=" padding:0; margin:0; ">
+                     <div class="read-more" style=" padding:0; margin:0; ">
+                         <p style="color:grey;">Kind of an extreme sport and definitely not for the faint of heart.<br>
+                             But, if you live for adventure then maybe climbing a mountain of
+                             a low or high peak is exactly what you want to do. </p>
+                     </div>
+                 </div>
+
+
+             </td>
+         </tr>
+         <tr style="text-align:left">
+             <td>
+                 <img class="img-round" src="images/kayaking.jpg" alt="Avatar" align="left"
+                      style="width:128px;height:128px" hspace="20"/>
+                 <p style="color:black;"><strong>Kayaking</strong></p>
+                 <div class="content1" style=" padding:0; margin:0; ">
+                     <div class="read-more" style=" padding:0; margin:0; ">
+                         <p style="color:grey;"> Again, for those that love adventure.<br>
+                             Once you try kayaking, we’re positive you’ll love it.
+                             Kayaking provides a number of
 										 advantages that make it unique .</p>
 				</div>
 				</div>
