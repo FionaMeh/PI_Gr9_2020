902ea08865ceec9d89a3adf337c06dc7e12d8369
Fortuna123 <58559930+Fortuna123@users.noreply.github.com>
2020-05-17 22:50:21 +0200

    Update Welcome.php

--- Welcome.php
+++ Welcome.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
-echo "Welcome:<br>".$_SESSION['email'];
-echo "<a href='logout.php'><br> LogOut</a>";
+//session_start();
+//echo "Welcome:<br>".$_SESSION['email'];
+//echo "<a href='logout.php'><br> LogOut</a>";
 ?>
