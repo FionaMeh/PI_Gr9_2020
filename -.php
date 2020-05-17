2a0fab93c41c9b3d30c082f88fb30bcf9c7a44dc
Fortuna123 <58559930+Fortuna123@users.noreply.github.com>
2020-05-17 22:57:41 +0200

    login i vjeter

---
+++ -.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+//<style type="text/css">
+//th
+//{
+//text:aslign:right;
+//}
+//h3
+//{
+//text-align:center;
+//}
+//</style>
+//</head>
+<body>
+//<table cellpadding="10" cellspacing="20" align="center">
+//<form method="post" action="Validim.php">
+//<tr><th>Email</th><td><input type="text id="email" name="email"></td></tr>
+//<tr><th>Password</th><td><input type="password" id="pass" name="password"></td></tr>
+//<tr><td colspan="5" align="center"><input type="checkbox" name="remember" value="1">Remember Me</td></tr>
+//<tr><td colspan="5" align="right"><input type="submit" value="Login" name="login"></td></tr>
+//</form>
+//</table>
+<?php
+//if(isset($_COOKIE['email'])and isset($_COOKIE['pass'])){
+//$email=$_COOKIE['email'];
+//$pass=$_COOKIE['pass'];
+//echo "<script>
+//document.getElementeById('email').value = '$email';
+//document.getElementeById('pass').value = '$pass';
+//</script>";
+}
+?>
+</body>
+</html>
