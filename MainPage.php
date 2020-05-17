96d81be97a6152d83078427e0eba5df3043a4e19
drenu83 <45490901+drenu83@users.noreply.github.com>
2020-05-17 14:42:18 +0200

    Update MainPage.php

--- MainPage.php
+++ MainPage.php
@@ -1,3 +1,23 @@
+<?php
+$apiKey = "2491caab176f88418b3a29767a2cd441";
+$cityId = "786714";
+$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
+
+?>
 <!DOCTYPE html>
 <html>
 <head lang="en">
